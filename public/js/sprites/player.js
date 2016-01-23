905e31203b193ded8cb04cc5f72ce4cccb0f7539
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-23 12:40:05 -1000

    first animation

--- public/js/sprites/player.js
+++ public/js/sprites/player.js
@@ -1,17 +1,36 @@
-// sprite class constructor
-JumpPunch.Player = function (game, id, name) {
-  this.game = game;
-  this.id = id;
-  this.name = name? name : 'Player '+(id+1);
+// An IIFE is a self-invoking function. (Immediately Invoked Function Expression)
+(function () {
 
-  // super constructor call
-  Phaser.Sprite.call(this, game, 0, 0, JumpPunch.ASSETS.SPRITESHEET.PLAYER.name);
-};
+  // private static variable
+  var ANIMATIONS = {
+    IDLE : {
+      name : 'title',
+      frames : [0,1,2,3],
+      fps : 5
+    }
+  };
 
-//extend Sprite prototype
-JumpPunch.Player.prototype = Object.create(Phaser.Sprite.prototype, {
-  constructor: {
-    value: JumpPunch.Player
-  }
-});
+  // sprite class constructor
+  JumpPunch.Player = function (game, id, name) {
+    this.game = game;
+    this.id = id;
+    this.name = name? name : 'Player '+(id+1);
 
+    // super constructor call
+    Phaser.Sprite.call(this, game, 0, 0, JumpPunch.ASSETS.SPRITESHEET.PLAYER.name);
+
+    // set animations
+    this.animations.add(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.frames);
+
+    // play the initial animation
+    this.animations.play(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.fps, true); // third argument is 'loop'
+  };
+
+  //extend Sprite prototype
+  JumpPunch.Player.prototype = Object.create(Phaser.Sprite.prototype, {
+    constructor: {
+      value: JumpPunch.Player
+    }
+  });
+
+})();
