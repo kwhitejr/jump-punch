24e10286e5a0d60b0279da62c2ae6b8889034845
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-23 11:52:17 -1000

    programmatically preload assets

--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -4,6 +4,20 @@ JumpPunch.Boot = function () {
 
 JumpPunch.Boot.prototype.preLoad = function () {
   //will preload all assets
+  //using functional and declarative programming, not imperatives!
+
+  Object.keys(JumpPunch.ASSETS).forEach(function(type) {
+    for (var asset in JumpPunch.ASSETS[type]) {
+      JumpPunch.game.load[ type.toLowerCase() ](
+        JumpPunch.ASSETS[type][ asset ].name,
+        JumpPunch.ASSETS[type][ asset ].width,
+        JumpPunch.ASSETS[type][ asset ].height,
+        JumpPunch.ASSETS[type][ asset ].frames,
+        JumpPunch.ASSETS[type][ asset ].path
+      );
+    }
+  });
+
 };
 
 JumpPunch.Boot.prototype.create = function () {
