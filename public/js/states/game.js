474957fbb9dcf94bc357b2f3c12406100c3667a1
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-23 10:45:16 -1000

    had to correct file placement in directories because i'm an idiot

---
+++ public/js/states/game.js
@@ -0,0 +1,15 @@
+JumpPunch.Game = function () {
+
+};
+
+JumpPunch.Game.prototype.create = function () {
+
+};
+
+JumpPunch.Game.prototype.udpate = function () {
+
+};
+
+JumpPunch.Game.prototype.shutdown = function () {
+
+};
